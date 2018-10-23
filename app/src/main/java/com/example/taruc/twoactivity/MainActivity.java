bd875994f9434d52b533a2f1d588721fe832ce20
lowyh <lowyh-ja16@student.tarc.edu.my>
2018-10-23 11:31:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/twoactivity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.taruc.twoactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG,"Button clicked!");
+    }
+}
